5ab30d3efa0ca1efd65fd75861331807e521a006
jdabassett <jacobbassett@gmail.com>
2023-01-18 17:14:22 -0700

    added sold out div to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import Star from "../images/star.png";
 
+// id: 1,
+// title: "Life Lessons with Katie Zaferes",
+// description: "I
+// price: 136,
+// coverImg: "katie-zaferes.png",
+// stats: {
+//     rating: 5.0,
+//     reviewCount: 6
+// },
+// location: "Online",
+// openSpots: 0,
+
 function Card(props) {
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img
         className="card--image"
-        src={require("../images/" + props.img)}
-        alt={"photo of " + props.img}
+        src={require("../images/" + props.item.coverImg)}
+        alt={"photo of " + props.item.coverImg}
       />
       <div className="card--stats">
         <img className="card--star" src={Star} alt="card one star" />
-        <span>{props.rating}</span>
-        <span className="faint reviews">({props.reviewCount}) • </span>
-        <span className="faint">{props.country}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="faint reviews">
+          ({props.item.stats.reviewCount}) •{" "}
+        </span>
+        <span className="faint">{props.item.location}</span>
       </div>
 
-      <p className="card--summary">{props.title}</p>
+      <p className="card--summary">{props.item.title}</p>
       <p className="card--pricing">
-        <strong className="bold">From ${props.price}</strong>/person
+        <strong className="bold">From ${props.item.price}</strong>/person
       </p>
     </div>
   );
